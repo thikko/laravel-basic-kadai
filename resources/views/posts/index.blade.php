f1663d8a45f179e8659d508aa5af03da1dae6df5
thikko <aosaku706@icloud.com>
2024-09-30 20:30:21 +0900

    課題作成2

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>laravel課題</title>
+</head>
+
+<body>
+  <h1>投稿一覧</h1>
+</body>
+</html>
